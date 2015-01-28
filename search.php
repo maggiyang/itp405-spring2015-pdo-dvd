70d6f2f5ae5392bb12073b14d06e2e31843af6f3
maggiyang <maggiyan@usc.edu>
2015-01-27 20:49:11 -0800

    style search results

--- search.php
+++ search.php
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>DVD Search using PDO</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
 <?php
 
 //check for errors when loading search.php, it redirects to home or index.php
@@ -37,13 +77,13 @@ $like = '%' . $dvd . '%';
 $statement->bindParam(1, $like); 
 $statement->execute();  //executes statement
 $titles = $statement->fetchAll(PDO::FETCH_OBJ); 
-//var_dump($titles);  //dump info on any variable
-
-echo "You searched for '$dvd'"; 
 
 ?>
 
 <?php if ($statement->rowCount() > 0) : ?>
+    <div class="container">
+    <div class="starter-template">
+    <h1>You searched for "<?php echo $dvd ?>"</h1>
     <table class="table table-hover">
         <tr>
             <th>Title</th>
@@ -69,9 +109,10 @@ echo "You searched for '$dvd'";
     <br> 
     <p>Your search did not match any results</p>
     <a href="index.php">Return to search menu</a>
+    </div>
+    </div>
 <?php endif ?>
-
-
-
+</body>
+</html>
 
 
